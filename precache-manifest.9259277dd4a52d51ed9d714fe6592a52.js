63d50842a9715313faf13432804dd1208a08c96e
Shovon Das <shovondas26@gmail.com>
2019-05-15 20:59:22 +0600

    Updates

---
+++ precache-manifest.9259277dd4a52d51ed9d714fe6592a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b857b4dff0cf26bf2be41da001e695",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "edc7d35d795808103af2",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a94eb8988857975d2e62",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "edc7d35d795808103af2",
+    "url": "/contactmanager/static/js/2.c3800c45.chunk.js"
+  },
+  {
+    "revision": "a94eb8988857975d2e62",
+    "url": "/contactmanager/static/js/main.a696bfef.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
